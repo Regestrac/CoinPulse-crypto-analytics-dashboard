c57bb5db0b3b9ff76d01ae8e72b6ba2e88b19c80
Sojo C Johny <sojocjohny37@gmail.com>
2025-11-26 13:58:26 +0530

    Created a basic outline for home page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import Image from 'next/image'
 
 const page = () => {
   return (
-    <p className='text-3xl text-indigo-500'>
-      CoinPulse
-    </p>
+    <main className='main-container'>
+      <section className='home-grid'>
+        <div id="coin-overview">
+          <div className="header pt-2">
+            <Image src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png" alt='' width={56} height={56} />
+            <div className='info'>
+              <p>Bitcoin / BTC</p>
+              <h1>$89,133.00</h1>
+            </div>
+          </div>
+        </div>
+        <p>Trending Coins</p>
+      </section>
+      <section className="w-full mt-7 space-y-4">
+        Categories
+      </section>
+    </main>
   )
 }
 
