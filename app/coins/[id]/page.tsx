090a59726c4d12344551f513c6015ff5acd72ee9
Sojo C Johny <sojocjohny37@gmail.com>
2025-12-04 13:57:25 +0530

    Integrated API for coin details section and style updates

--- app/coins/[id]/page.tsx
+++ app/coins/[id]/page.tsx
@@ -1,33 +1,47 @@
+import { fetcher } from "@/lib/coingecko.actions";
+import { formatCurrency } from "@/lib/utils";
 import { ArrowUpRight } from "lucide-react";
 import Link from "next/link";
 
-const coinDetails = [
-  {
-    label: 'Market Cap',
-    value: '$1234567',
-    link: '',
-    linkText: '',
-  },
-  {
-    label: 'Market Cap',
-    value: '$1234567',
-  },
-  {
-    label: 'Market Cap',
-    value: '$1234567',
-  },
-  {
-    label: 'Market Cap',
-    value: '$1234567',
-  },
-  {
-    label: 'Market Cap',
-    value: '$1234567',
-  },
-]
-
 const CoinDetails = async ({ params }: NextPageProps) => {
   const { id } = await params;
+
+  const coinData = await fetcher<CoinDetailsData>(`/coins/${id}`, {
+    dex_pair_format: 'contract_address',
+  });
+
+  const coinDetails = [
+    {
+      label: 'Market Cap',
+      value: formatCurrency(coinData.market_data.market_cap.usd),
+    },
+    {
+      label: 'Market Cap Rank',
+      value: `# ${coinData.market_cap_rank}`,
+    },
+    {
+      label: 'Total Volume',
+      value: formatCurrency(coinData.market_data.total_volume.usd),
+    },
+    {
+      label: 'Website',
+      value: '-',
+      link: coinData.links.homepage[0],
+      linkText: 'Homepage',
+    },
+    {
+      label: 'Explorer',
+      value: '-',
+      link: coinData.links.blockchain_site[0],
+      linkText: 'Explorer',
+    },
+    {
+      label: 'Community',
+      value: '-',
+      link: coinData.links.subreddit_url,
+      linkText: 'Community',
+    },
+  ];
 
   return (
     <main id="coin-details-page">
@@ -44,25 +58,28 @@ const CoinDetails = async ({ params }: NextPageProps) => {
       </section>
 
       <section className="secondary">
-        <h4>Coin details</h4>
-        <ul className="details-grid">
-          {coinDetails.map(({ label, link, value, linkText }, index) => (
-            <li key={index}>
-              <p className={label}>{label}</p>
+        <div className="details">
+          <h4>Coin Details</h4>
 
-              {link ? (
-                <div className="link">
-                  <Link href={link} target="_blank">
-                    {linkText || label}
-                  </Link>
-                  <ArrowUpRight size={16} />
-                </div>
-              ) : (
-                <p className="text-base font-medium">{value}</p>
-              )}
-            </li>
-          ))}
-        </ul>
+          <ul className="details-grid">
+            {coinDetails.map(({ label, value, link, linkText }, index) => (
+              <li key={index}>
+                <p className={label}>{label}</p>
+
+                {link ? (
+                  <div className="link">
+                    <Link href={link} target="_blank">
+                      {linkText || label}
+                    </Link>
+                    <ArrowUpRight size={16} />
+                  </div>
+                ) : (
+                  <p className="text-base font-medium">{value}</p>
+                )}
+              </li>
+            ))}
+          </ul>
+        </div>
       </section>
     </main>
   )
